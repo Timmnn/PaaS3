8bee75e64d04cca43ce67454a8d06dd8fe04674d
timmn <timmmmnn@gmail.com>
2024-07-30 22:44:50 +0200

    Fix in docker compose

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,13 @@ touch ./backend/.env
 cat .env.example > .env
 
 
+
+
+bash ./scripts/docker_compose_run.sh
+
 npm i -g drizzle-kit
 (
    cd backend/drizzle &&
    bun install drizzle-orm drizzle-kit pg &&
    bash ./sync_schema.sh
 )
-
-bash ./scripts/docker_compose_run.sh
